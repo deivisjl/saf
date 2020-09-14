c20d5edddb93d3e5a86cf9ad7533a679cf8f6027
deivisjl <deivisjl@gmail.com>
2020-09-14 15:20:40 -0600

    Refactorizacion de template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,9 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -16,30 +16,21 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="sidebar-mini layout-fixed layout-navbar-fixed text-sm">
+<body class="text-sm">
     <div id="app">
-        <div class="wrapper">
-          <!-- navbar -->
-              @include('layouts.includes.navbar')
-          <!-- end navar -->
-          <!-- sidebar -->
-              @include('layouts.includes.sidebar')
-          <!-- end sidebar -->
-
-          <!-- content-wrapper -->
-          <div class="content-wrapper">
-             @yield('content')
-          </div>
-          <!-- end content-wrapper -->
-          <!-- footer -->
-          <footer class="main-footer">
-              <strong>Copyright © 2020 <a href="/">SAF</a>.</strong>
-                All rights reserved.
-          </footer>
-          <!-- footer -->
+        <div class="container-logo">
+            <div id="logo">
+                 <img src="{{ asset('/img/funeraria-logo.png') }}">
+            </div>
         </div>
+        <!-- navbar -->
+        @include('layouts.includes.navbar')
+        
+          <div class="container-fluid content">
+               @yield('content')
+          </div>
     </div>
-    <!-- Scripts -->
+      <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('js')
 </body>
--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -1,33 +1,40 @@
-<!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-dark navbar-lightblue">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <!-- <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li> -->
-    </ul>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
+  <a class="navbar-brand" href="/">Inicio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Accesos
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('permisos.index') }}">Permisos</a>
+            <a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
+            <a class="dropdown-item" href="{{ route('usuarios-roles.index') }}">Asignar roles</a>
+          </div>
+      </li>
+      <li class="nav-item active">
+        {{-- <a class="nav-link" href="#">Home</a> --}}
+      </li>
+    </ul>
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
+  <ul class="navbar-nav ml-auto">
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fas fa-th-large"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a class="nav-link btn btn-outline-info" data-toggle="dropdown" href="#">
+             {{ Auth::user()->nombres }} {{ Auth::user()->apellidos }}
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <span class="dropdown-item dropdown-header">Más opciones</span>
           <div class="dropdown-divider"></div>
           <a href="{{ route('logout') }}" class="dropdown-item">
-            <i class="fas fa-sign-out-alt mr2"></i> Cerrar sesión
+              <i class="fas fa-sign-out-alt mr2"></i> Cerrar sesión
           </a>
-        </div>
+          </div>
       </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+      </ul>
+  </div>
+</nav>
